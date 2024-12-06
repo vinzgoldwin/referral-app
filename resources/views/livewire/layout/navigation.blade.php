a9cac15b8018a41e01017dc3a61ce1687a8d463f
vinzgoldwin <vinzgoldwin@gmail.com>
2024-12-06 14:18:46 +0700

    fix can not log out

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -1,3 +1,13 @@
+<?php
+
+use App\Livewire\Actions\Logout;
+$logout = function (Logout $logout) {
+    $logout();
+    $this->redirect('/', navigate: true);
+};
+?>
+
+
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
